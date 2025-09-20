2544146ef63c3ddfc385cdd004683c456098e741
Stas <stas@frenchcake.ru>
2025-09-20 15:31:42 +0400

    Обновлена база данных начинок под реальное производство
    - Заменены все 27 начинок на реальные из кондитерской
    - Добавлены точные плотности на основе измерений
    - Начинки сгруппированы по категориям: ягодные, шоколадные, карамельные, муссовые и др.
    - Добавлены уникальные начинки: 3D карамель, круассан с кофе, сент-оноре, эстерхази
    - Все плотности рассчитаны по реальным образцам продукции

--- fillings.js
+++ fillings.js
@@ -1,248 +1,248 @@
-// База данных начинок для тортов
-// Каждая начинка имеет: название, плотность (г/см³), стандартные параметры
+// База данных начинок для кондитерской FrenchCake
+// Реальные начинки с измеренной плотностью
 const FILLINGS_DATABASE = {
-    "biskuit_vanilla": {
-        name: "Бисквит ванильный",
-        density: 0.45, // г/см³
-        category: "Бисквитные",
-        description: "Классический ванильный бисквит",
-        standardHeight: 6, // см
-        standardDiameter: 20, // см для круглого
-        standardWeight: 0.1 // кг (приблизительно)
-    },
-    "biskuit_chocolate": {
-        name: "Бисквит шоколадный",
-        density: 0.48,
-        category: "Бисквитные", 
-        description: "Насыщенный шоколадный бисквит",
+    "malinovaya": {
+        name: "Малиновая",
+        density: 0.721791125,
+        category: "Ягодные",
+        description: "Нежная малиновая начинка",
         standardHeight: 6,
         standardDiameter: 20,
         standardWeight: 0.1
     },
-    "sponge_classic": {
-        name: "Губка классическая",
-        density: 0.35,
-        category: "Губчатые",
-        description: "Легкая воздушная основа",
-        standardHeight: 5,
-        standardDiameter: 18,
-        standardWeight: 0.09
-    },
-    "choux_cream": {
-        name: "Заварное тесто",
-        density: 0.55,
-        category: "Заварные",
-        description: "Заварная основа для эклеров и профитролей",
-        standardHeight: 4,
-        standardDiameter: 15,
-        standardWeight: 0.08
-    },
-    "shortbread": {
-        name: "Песочное тесто",
-        density: 0.65,
-        category: "Песочные",
-        description: "Рассыпчатое песочное тесто",
-        standardHeight: 3,
-        standardDiameter: 22,
-        standardWeight: 0.12
-    },
-    "puff_pastry": {
-        name: "Слоеное тесто",
-        density: 0.58,
-        category: "Слоеные",
-        description: "Многослойное слоеное тесто",
-        standardHeight: 4,
-        standardDiameter: 20,
-        standardWeight: 0.11
-    },
-    "honey_cake": {
-        name: "Медовый корж",
-        density: 0.52,
-        category: "Медовые",
-        description: "Традиционный медовый корж",
-        standardHeight: 5,
+    "limonnaya_vanil": {
+        name: "Лимонная ваниль",
+        density: 0.721791125,
+        category: "Цитрусовые",
+        description: "Сочетание лимона и ванили",
+        standardHeight: 6,
         standardDiameter: 20,
         standardWeight: 0.1
     },
-    "napoleon": {
-        name: "Наполеон",
-        density: 0.42,
-        category: "Слоеные",
-        description: "Тонкие коржи для торта Наполеон",
-        standardHeight: 3,
-        standardDiameter: 22,
-        standardWeight: 0.09
+    "nezhnaya_vanil": {
+        name: "Нежная ваниль",
+        density: 0.591737488,
+        category: "Ванильные",
+        description: "Классическая нежная ваниль",
+        standardHeight: 6,
+        standardDiameter: 20,
+        standardWeight: 0.1
     },
-    "red_velvet": {
+    "krasny_barhat": {
         name: "Красный бархат",
-        density: 0.46,
+        density: 0.649612013,
         category: "Бисквитные",
-        description: "Нежный красный бисквит",
+        description: "Знаменитый красный бархатный торт",
         standardHeight: 6,
         standardDiameter: 20,
         standardWeight: 0.1
     },
-    "carrot_cake": {
-        name: "Морковный бисквит",
-        density: 0.53,
+    "shokoladny_biskuit": {
+        name: "Шоколадный бисквит",
+        density: 0.608273066,
+        category: "Шоколадные",
+        description: "Классический шоколадный бисквит",
+        standardHeight: 6,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "dvoynoy_shokolad": {
+        name: "Двойной шоколад",
+        density: 0.721791125,
+        category: "Шоколадные",
+        description: "Интенсивный двойной шоколад",
+        standardHeight: 6,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "medovik": {
+        name: "Медовик",
+        density: 0.649612013,
+        category: "Медовые",
+        description: "Традиционный медовый торт",
+        standardHeight: 5,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "banan_finik": {
+        name: "Банан-финик",
+        density: 0.649612013,
         category: "Фруктовые",
-        description: "Влажный морковный бисквит",
+        description: "Экзотическое сочетание банана и фиников",
+        standardHeight: 6,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "fistashka_malina": {
+        name: "Фисташка малина",
+        density: 0.647013565,
+        category: "Ореховые",
+        description: "Изысканное сочетание фисташек и малины",
+        standardHeight: 5,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "solenaya_karamel": {
+        name: "Соленая карамель",
+        density: 0.590556375,
+        category: "Карамельные",
+        description: "Модная соленая карамель",
+        standardHeight: 6,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "esterhazy": {
+        name: "Эстерхази",
+        density: 0.487209009,
+        category: "Ореховые",
+        description: "Венгерский ореховый торт",
+        standardHeight: 4,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "snikers": {
+        name: "Сникерс",
+        density: 0.649612013,
+        category: "Карамельные",
+        description: "Как знаменитый батончик",
+        standardHeight: 6,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "karamel_3d": {
+        name: "3D карамель",
+        density: 0.928017161,
+        category: "Карамельные",
+        description: "Объемная карамельная начинка",
         standardHeight: 7,
         standardDiameter: 20,
-        standardWeight: 0.11
+        standardWeight: 0.1
     },
-    "lemon_cake": {
-        name: "Лимонный бисквит",
-        density: 0.44,
-        category: "Цитрусовые",
-        description: "Легкий лимонный бисквит",
-        standardHeight: 6,
-        standardDiameter: 18,
-        standardWeight: 0.09
+    "shokolad_3d": {
+        name: "3D шоколад",
+        density: 0.928017161,
+        category: "Шоколадные",
+        description: "Объемная шоколадная начинка",
+        standardHeight: 7,
+        standardDiameter: 20,
+        standardWeight: 0.1
     },
-    "coconut_cake": {
-        name: "Кокосовый бисквит",
-        density: 0.47,
-        category: "Ореховые",
-        description: "Ароматный кокосовый бисквит",
+    "muss_mango_marakuyya": {
+        name: "Мусс манго маракуйя",
+        density: 0.590556375,
+        category: "Муссовые",
+        description: "Легкий тропический мусс",
+        standardHeight: 8,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "klubnichno_yogurt": {
+        name: "Клубнично йогуртовый",
+        density: 0.649612013,
+        category: "Ягодные",
+        description: "Свежий клубнично-йогуртовый",
         standardHeight: 6,
         standardDiameter: 20,
         standardWeight: 0.1
     },
-    "almond_cake": {
-        name: "Миндальный бисквит",
-        density: 0.51,
-        category: "Ореховые",
-        description: "Нежный миндальный бисквит",
+    "sent_onore": {
+        name: "Сент-оноре",
+        density: 0.721791125,
+        category: "Заварные",
+        description: "Классический французский торт",
+        standardHeight: 8,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "morkovnaya": {
+        name: "Морковная",
+        density: 0.721791125,
+        category: "Овощные",
+        description: "Влажный морковный торт",
+        standardHeight: 7,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "kruassan_kofe": {
+        name: "Круассан с кофе",
+        density: 1.082686688,
+        category: "Кофейные",
+        description: "Слоеная кофейная начинка",
+        standardHeight: 6,
+        standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "madlen_ris_puding": {
+        name: "Мадлен с рисовым пудингом",
+        density: 0.721791125,
+        category: "Заварные",
+        description: "Нежный мадлен с рисовым кремом",
         standardHeight: 5,
         standardDiameter: 18,
         standardWeight: 0.1
     },
-    "walnut_cake": {
-        name: "Ореховый бисквит",
-        density: 0.54,
-        category: "Ореховые",
-        description: "Насыщенный ореховый вкус",
-        standardHeight: 6,
-        standardDiameter: 20,
-        standardWeight: 0.11
-    },
-    "coffee_cake": {
-        name: "Кофейный бисквит",
-        density: 0.49,
-        category: "Кофейные",
-        description: "Ароматный кофейный бисквит",
-        standardHeight: 6,
-        standardDiameter: 20,
-        standardWeight: 0.1
-    },
-    "tiramisu_base": {
-        name: "Основа Тирамису",
-        density: 0.41,
-        category: "Кофейные",
-        description: "Легкая основа для тирамису",
-        standardHeight: 4,
-        standardDiameter: 18,
-        standardWeight: 0.08
-    },
-    "cheesecake_base": {
-        name: "Основа чизкейка",
-        density: 0.68,
+    "klubnichny_cheesecake": {
+        name: "Клубничный чизкейк",
+        density: 0.928017161,
         category: "Творожные",
-        description: "Плотная творожная основа",
+        description: "Плотный клубничный чизкейк",
         standardHeight: 8,
         standardDiameter: 22,
         standardWeight: 0.15
     },
-    "brownie_base": {
-        name: "Браuni основа",
-        density: 0.62,
-        category: "Шоколадные",
-        description: "Плотная шоколадная основа",
+    "vishnevy_makarons": {
+        name: "Вишневый макаронс",
+        density: 0.649612013,
+        category: "Ягодные",
+        description: "Нежный вишневый макаронс",
         standardHeight: 4,
+        standardDiameter: 18,
+        standardWeight: 0.1
+    },
+    "cherny_les": {
+        name: "Черный лес",
+        density: 0.590556375,
+        category: "Шоколадные",
+        description: "Классический черный лес",
+        standardHeight: 6,
         standardDiameter: 20,
+        standardWeight: 0.1
+    },
+    "tryufel": {
+        name: "Трюфель",
+        density: 0.781564453,
+        category: "Шоколадные",
+        description: "Богатый шоколадный трюфель",
+        standardHeight: 5,
+        standardDiameter: 18,
+        standardWeight: 0.1
+    },
+    "napoleon": {
+        name: "Наполеон",
+        density: 1.082686688,
+        category: "Слоеные",
+        description: "Классический наполеон",
+        standardHeight: 6,
+        standardDiameter: 22,
         standardWeight: 0.12
     },
-    "fruit_sponge": {
-        name: "Фруктовая губка",
-        density: 0.38,
-        category: "Фруктовые",
-        description: "Легкая основа с фруктами",
-        standardHeight: 5,
-        standardDiameter: 18,
-        standardWeight: 0.09
-    },
-    "berry_cake": {
-        name: "Ягодный бисквит",
-        density: 0.43,
-        category: "Ягодные",
-        description: "Нежный бисквит с ягодами",
+    "toffi_plombir": {
+        name: "Тоффи пломбир",
+        density: 0.649612013,
+        category: "Карамельные",
+        description: "Кремовый тоффи-пломбир",
         standardHeight: 6,
         standardDiameter: 20,
         standardWeight: 0.1
     },
-    "banana_cake": {
-        name: "Банановый бисквит",
-        density: 0.5,
-        category: "Фруктовые",
-        description: "Влажный банановый бисквит",
-        standardHeight: 7,
-        standardDiameter: 20,
-        standardWeight: 0.11
-    },
-    "orange_cake": {
-        name: "Апельсиновый бисквит",
-        density: 0.45,
-        category: "Цитрусовые",
-        description: "Яркий апельсиновый вкус",
-        standardHeight: 6,
-        standardDiameter: 18,
-        standardWeight: 0.09
-    },
-    "strawberry_cake": {
-        name: "Клубничный бисквит",
-        density: 0.44,
-        category: "Ягодные",
-        description: "Нежный клубничный бисквит",
-        standardHeight: 6,
-        standardDiameter: 20,
-        standardWeight: 0.1
-    },
-    "pistachio_cake": {
-        name: "Фисташковый бисквит",
-        density: 0.52,
-        category: "Ореховые",
-        description: "Изысканный фисташковый вкус",
-        standardHeight: 5,
-        standardDiameter: 18,
-        standardWeight: 0.1
-    },
-    "matcha_cake": {
-        name: "Бисквит матча",
-        density: 0.46,
-        category: "Чайные",
-        description: "Зеленый чай матча",
-        standardHeight: 6,
-        standardDiameter: 18,
-        standardWeight: 0.1
-    },
-    "rum_cake": {
-        name: "Ромовый бисквит",
-        density: 0.48,
-        category: "Алкогольные",
-        description: "Ароматный ромовый бисквит",
-        standardHeight: 6,
-        standardDiameter: 20,
-        standardWeight: 0.1
-    },
-    "lavender_cake": {
-        name: "Лавандовый бисквит",
-        density: 0.43,
-        category: "Цветочные",
-        description: "Деликатный лавандовый аромат",
-        standardHeight: 5,
-        standardDiameter: 18,
-        standardWeight: 0.09
+    "ekler": {
+        name: "Эклер",
+        density: 0.721791125,
+        category: "Заварные",
+        description: "Заварная основа эклера",
+        standardHeight: 4,
+        standardDiameter: 15,
+        standardWeight: 0.08
     }
 };
 
